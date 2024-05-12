a9d3c4c41586ab8c1f93e89299831c82a65fca6c
onurdrsn <askers542@gmail.com>
2024-05-12 20:20:23 +0300

    up

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,14 +3,21 @@ export default [
         title: 'Termainal WebSite',
         imgUrl: '',
         stack: ['HTML', 'CSS', 'JS'],
-        link: 'fkcodes.com',
-        description: `This is my personal website that I created using React and TailwindCSS.
+        link: 'termui.netlify.com',
+        description: "This is a terminal website that I created using HTML, CSS and JS.",
     },
     {
-        title: 'Termainal WebSite',
+        title: 'Portfolio WebSite',
         imgUrl: '',
         stack: ['React', 'TailwindCSS', 'Vite'],
         link: 'onurdrsn.netlify.com',
-        description: `This is my personal website that I created using React and TailwindCSS.
+        description: "This is my personal website that I created using React and TailwindCSS.",
+    },
+    {
+        title: 'Old Portfolio WebSite',
+        imgUrl: '',
+        stack: ['PHP', 'CSS', 'JS'],
+        link: 'onur-dursun.epizy.com',
+        description: "This is my old personal website that I created using PHP, CSS and JS.",
     },
 ]
